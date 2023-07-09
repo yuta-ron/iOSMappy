22256928826b17e7269d008db95cae65f82fedd8
yuta-ron <jan.joqr@gmail.com>
2023-07-09 11:26:11 +0900

    add: debug

--- iOSMappy/ViewController.swift
+++ iOSMappy/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var locationInfoLabel: UILabel!
+    
+    let locationManager = CLLocationManager();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
     }
-
-
+    
+    @IBAction func getCurrentLocationTapped(_ sender: Any) {
+        print("tapped")
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let loc = locations.last else { return }
+        
+        CLGeocoder().reverseGeocodeLocation(loc, completionHandler: {(placemarks, error) in
+            
+            if let error = error {
+                print("reverseGeocodeLocation Failed: \(error.localizedDescription)")
+                return
+            }
+            
+            if let placemark = placemarks?[0] {
+                
+                var locInfo = ""
+                locInfo = locInfo + "Latitude: \(loc.coordinate.latitude)\n"
+                locInfo = locInfo + "Longitude: \(loc.coordinate.longitude)\n\n"
+                
+                locInfo = locInfo + "Country: \(placemark.country ?? "")\n"
+                locInfo = locInfo + "State/Province: \(placemark.administrativeArea ?? "")\n"
+                locInfo = locInfo + "City: \(placemark.locality ?? "")\n"
+                locInfo = locInfo + "PostalCode: \(placemark.postalCode ?? "")\n"
+                locInfo = locInfo + "Name: \(placemark.name ?? "")"
+                
+                self.locationInfoLabel.text = locInfo
+            }
+        })
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error: \(error.localizedDescription)")
+    }
 }
 
